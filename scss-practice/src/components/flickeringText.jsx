9568e0ee568074feb993b3e692a3c20f6624ac00
Marc Philippe Beaujean <marcphilippebeaujean@marcs-mbp.home>
2018-09-16 15:30:10 +0100

    finished first project, light flickering animations now working

--- scss-practice/src/components/flickeringText.jsx
+++ scss-practice/src/components/flickeringText.jsx
@@ -8,14 +8,20 @@ class FlickeringTextComponent extends Component {
         <div id="word-vertical-center">
           <div id="word">
             <div className="letter" id="letter-H">
-              <div className="letter-component letter-H-side" />
-              <div className="letter-component" id="letter-H-middle" />
-              <div className="letter-component letter-H-side" />
+              <div className="letter-component letter-H-side letter-component-1" />
+              <div
+                className="letter-component  letter-component-2"
+                id="letter-H-middle"
+              />
+              <div className="letter-component letter-H-side letter-component-3" />
             </div>
             <div className="letter" id="letter-I">
-              <div className="letter-component letter-I-top" />
-              <div className="letter-component" id="letter-I-middle" />
-              <div className="letter-component letter-I-top" />
+              <div className="letter-component letter-I-top letter-component-4" />
+              <div
+                className="letter-component letter-component-5"
+                id="letter-I-middle"
+              />
+              <div className="letter-component letter-I-top letter-component-6" />
             </div>
           </div>
         </div>
