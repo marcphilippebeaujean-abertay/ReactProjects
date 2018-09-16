9ea4ac4ef89dc458800026713c608b7971c63daf
Marc Philippe Beaujean <marcphilippebeaujean@marcs-mbp.home>
2018-09-16 14:14:33 +0100

    centered the word

--- scss-practice/src/components/flickeringText.jsx
+++ scss-practice/src/components/flickeringText.jsx
@@ -5,16 +5,18 @@ class FlickeringTextComponent extends Component {
   render() {
     return (
       <div className="frame" id="hi-frame">
-        <div id="word">
-          <div className="letter" id="letter-H">
-            <div className="letter-component letter-H-side" />
-            <div className="letter-component" id="letter-H-middle" />
-            <div className="letter-component letter-H-side" />
-          </div>
-          <div className="letter" id="letter-I">
-            <div className="letter-component letter-I-top" />
-            <div className="letter-component" id="letter-I-base" />
-            <div className="letter-component letter-I-top" />
+        <div id="word-vertical-center">
+          <div id="word">
+            <div className="letter" id="letter-H">
+              <div className="letter-component letter-H-side" />
+              <div className="letter-component" id="letter-H-middle" />
+              <div className="letter-component letter-H-side" />
+            </div>
+            <div className="letter" id="letter-I">
+              <div className="letter-component letter-I-top" />
+              <div className="letter-component" id="letter-I-base" />
+              <div className="letter-component letter-I-top" />
+            </div>
           </div>
         </div>
       </div>
