d190b96b3f952c7767cefde0a6d19ddabe65c3b4
Marc Philippe Beaujean <marcphilippebeaujean@Marcs-MacBook-Pro.local>
2018-09-17 17:10:22 +0100

    created basic calculator and imported new font

---
+++ react-calcultor/src/components/calculator.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../css/style.css";
+
+export default class Calculator extends Component {
+  render() {
+    return (
+      <div id="calculator">
+        <p className="digit-display">0</p>
+      </div>
+    );
+  }
+}
