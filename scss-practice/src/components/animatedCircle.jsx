a1aacac9216dda32cb2c69714af5dd36670530e1
Marc Philippe Beaujean <marcphilippebeaujean@Marcs-MacBook-Pro.local>
2018-09-17 13:12:03 +0100

    finished animation

--- scss-practice/src/components/animatedCircle.jsx
+++ scss-practice/src/components/animatedCircle.jsx
@@ -10,12 +10,18 @@ class AnimatedCircle extends Component {
       <div className="frame" id="anim-circle-frame">
         <div id="circle">
           <div id="top-half">
-            <div className="circle-fragment" id="top-left" />
-            <div className="circle-fragment" id="top-right" />
+            <div className="circle-fragment circle-fragment-1" id="top-left" />
+            <div className="circle-fragment circle-fragment-2" id="top-right" />
           </div>
           <div id="bottom-half">
-            <div className="circle-fragment" id="bottom-left" />
-            <div className="circle-fragment" id="bottom-right" />
+            <div
+              className="circle-fragment  circle-fragment-3"
+              id="bottom-left"
+            />
+            <div
+              className="circle-fragment circle-fragment-4"
+              id="bottom-right"
+            />
           </div>
         </div>
       </div>
