6caf9c0bacc2960e00571b75bd37251e9fbdfd91
Marc Philippe Beaujean <marci.philippe@googlemail.com>
2018-09-23 22:53:34 +0100

    more specific styling for the games dropdowns

--- blog-frontend/src/components/navGamesDropdown.jsx
+++ blog-frontend/src/components/navGamesDropdown.jsx
@@ -5,10 +5,16 @@ export default class NavGamesDropdown extends Component {
   render() {
     return (
       <div id="games-dropdown">
-        <div className="nav-element project-dropdown-element secondary-dropdown-elem">
+        <div
+          className="nav-element project-dropdown-element scndry-dd"
+          id="she-town-dropdown"
+        >
           <p>she-town</p>
         </div>
-        <div className="nav-element project-dropdown-element secondary-dropdown-elem">
+        <div
+          className="nav-element project-dropdown-element scndry-dd"
+          id="indestructible-dropdown"
+        >
           <p>indestructible</p>
         </div>
       </div>
