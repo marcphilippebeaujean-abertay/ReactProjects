c711ed47333ebb6b010895364565af0dbfde7015
Marc Philippe Beaujean <marci.philippe@googlemail.com>
2018-09-23 23:03:37 +0100

    added new class for secondary dropdown elements

--- blog-frontend/src/components/navGamesDropdown.jsx
+++ blog-frontend/src/components/navGamesDropdown.jsx
@@ -4,15 +4,15 @@ import "../css/style.css";
 export default class NavGamesDropdown extends Component {
   render() {
     return (
-      <div id="games-dropdown">
+      <div className="scndry-dd" id="games-dropdown">
         <div
-          className="nav-element project-dropdown-element scndry-dd"
+          className="nav-element project-dropdown-element scndry-dd-elem"
           id="she-town-dropdown"
         >
           <p>she-town</p>
         </div>
         <div
-          className="nav-element project-dropdown-element scndry-dd"
+          className="nav-element project-dropdown-element scndry-dd-elem"
           id="indestructible-dropdown"
         >
           <p>indestructible</p>
