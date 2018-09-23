eebccb9697474b63ce61a88965207f1021a0f5fc
Marc Philippe Beaujean <marci.philippe@googlemail.com>
2018-09-23 23:08:51 +0100

    configured some nav bar media queries

--- blog-frontend/src/reducers/navigationReducer.js
+++ blog-frontend/src/reducers/navigationReducer.js
@@ -1,34 +1,11 @@
 const defaultState = {
-  currentURL: "home",
-  dropDownHoveredList: []
+  currentURL: "home"
 };
 
 const navigationReducer = (previousState = defaultState, action) => {
   let newState = { ...previousState };
   switch (action.type) {
-    case "DROPDOWN_HOVER":
-      let newHoverArr = [
-        ...previousState.dropDownHoveredList,
-        action.data.hoverLink
-      ];
-      newState.dropDownHoveredList = newHoverArr;
-      console.log(newState.dropDownHoveredList);
-      break;
-    case "DROPDOWN_END":
-      let indexToRemove = newState.dropDownHoveredList.indexOf(
-        action.data.hoverLink
-      );
-      if (indexToRemove >= 0) {
-        newState.dropDownHoveredList = [
-          ...previousState.dropDownHoveredList.slice(0, indexToRemove),
-          ...previousState.dropDownHoveredList.slice(indexToRemove + 1)
-        ];
-      } else {
-        console.log("called end hover although element is not in array");
-      }
-      break;
     default:
-      console.log("Strange navigation reducer action");
       break;
   }
   return newState;
