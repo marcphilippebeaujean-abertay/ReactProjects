02d5eff845d9d34c9e48fd982217fe74fb01e2d7
Marc Philippe Beaujean <marci.philippe@googlemail.com>
2018-09-29 22:44:22 +0100

    added hover effect to social media sidebar

--- blog-frontend/src/components/socialMedia.jsx
+++ blog-frontend/src/components/socialMedia.jsx
@@ -16,12 +16,17 @@ export default class SocialMediaSidebar extends Component {
             id="twitter-logo"
           />
         </a>
-        <img
-          src={GitLogo}
-          alt="GitHub logo"
-          className="social-media-logo"
-          id="github-logo"
-        />
+        <a
+          href="https://github.com/marcphilippebeaujean-abertay"
+          target="_blank"
+        >
+          <img
+            src={GitLogo}
+            alt="GitHub logo"
+            className="social-media-logo"
+            id="github-logo"
+          />
+        </a>
         <a
           href="https://www.linkedin.com/in/marc-philippe-beaujean-5ab27815a/"
           target="_blank"
