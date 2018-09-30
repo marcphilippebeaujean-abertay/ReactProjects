95e66979a77ddf002f667c806786595e0bf13ca3
Marc Philippe Beaujean <marci.philippe@googlemail.com>
2018-09-30 14:58:08 +0100

    updated styling for page

--- blog-frontend/src/components/Page Content/aboutInfoBar.jsx
+++ blog-frontend/src/components/Page Content/aboutInfoBar.jsx
@@ -6,9 +6,9 @@ export default class AboutInfoBar extends Component {
       <div>
         <h2 className="blog-post-header">About</h2>
         <p div="about-infotext-main">
-          Hi - my name is Marc Philippe Beaujean, welcome to my web page and
-          personal blog (coded and depolyed from scratch with leading frameworks
-          like ReactJS, Redux and Django)! I am currently studying at Abertay
+          My name is Marc Philippe Beaujean, welcome to my web page and personal
+          blog (coded and depolyed from scratch with leading frameworks like
+          ReactJS, Redux and Django)! I am currently studying at Abertay
           University in Scotland and looking for a job as a games, web or
           software developer. On my page, you will find information in the form
           of a blog posts and articles, links to most of my projects, and a
