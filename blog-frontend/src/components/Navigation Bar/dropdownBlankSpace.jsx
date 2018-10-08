375ec42d9343228819db84f3e590e26389a3bdb8
Marc Philippe Beaujean <marci.philippe@googlemail.com>
2018-10-08 13:18:48 +0100

    blank space component seems to be working well

--- blog-frontend/src/components/Navigation Bar/dropdownBlankSpace.jsx
+++ blog-frontend/src/components/Navigation Bar/dropdownBlankSpace.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
-
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 // Component that when hovered, will close the dropdown.
 // Allows for dynamic dropdown height creation without
 // having to adjust style of dropdown everytime a new
 // element is added.
 
-export default class DropdownBlankSpace extends Component {
+class DropdownBlankSpace extends Component {
   render() {
-    return <div className="dropdown-blank-space" />;
+    return (
+      <div
+        className="dropdown-blank-space"
+        onMouseEnter={() => this.props.onHovered()}
+      />
+    );
   }
 }
+
+DropdownBlankSpace.propTypes = {
+  onHovered: PropTypes.func.isRequired
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onHovered: () =>
+      dispatch({
+        type: "HIDE_DROPDOWN",
+        exitedViaHover: true
+      })
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(DropdownBlankSpace);
