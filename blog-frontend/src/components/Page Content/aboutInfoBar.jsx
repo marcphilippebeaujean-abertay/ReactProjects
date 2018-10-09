d682960016887e0fbfef8cf1817aad5da96fbddc
Marc Philippe Beaujean <marci.philippe@googlemail.com>
2018-10-09 22:51:54 +0100

    added flags to the about section

--- blog-frontend/src/components/Page Content/aboutInfoBar.jsx
+++ blog-frontend/src/components/Page Content/aboutInfoBar.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "../../css/style.css";
 import ReadMoreToggle from "../Tool Widgets/ReadMoreToggle";
+import ScottischFlag from "../../media/flags/scotland.jpg";
+import GermanyFlag from "../../media/flags/germany.png";
+import EnglishFlag from "../../media/flags/great_britain.png";
 
 export default class AboutInfoBar extends Component {
   render() {
@@ -16,13 +19,24 @@ export default class AboutInfoBar extends Component {
           <h3>About Me</h3>
           <div className="about-infotext-main">
             <ul className="about-list-keypoints">
-              <li>Country of Origin: Germany</li>
-              <li>Spoken Languages: Germany, English</li>
+              <li>
+                Country of Origin:{" "}
+                <img src={GermanyFlag} alt="Germany" className="flag" />
+              </li>
+              <li>
+                Spoken Languages:{" "}
+                <img src={GermanyFlag} alt="German, " className="flag" />{" "}
+                <img src={EnglishFlag} alt="English" className="flag" />
+              </li>
               <li>Date of Birth: 1996</li>
               <li>Programming Languages: C++, Python, JavaScript</li>
               <li>
                 Academic Qualifications: International Baccalaureate, BSc(Hons)
                 Computer Games Applications Development
+              </li>
+              <li>
+                University: Abertay University{" "}
+                <img src={ScottischFlag} alt="Scotland" className="flag" />
               </li>
             </ul>
           </div>
