1ff27ff496fd9d766b3a6786e064b3db99861040
Marc Philippe Beaujean <marci.philippe@googlemail.com>
2018-10-09 22:32:19 +0100

    fixed show hide button taking up the whole width

--- blog-frontend/src/components/Page Content/aboutInfoBar.jsx
+++ blog-frontend/src/components/Page Content/aboutInfoBar.jsx
@@ -7,21 +7,25 @@ export default class AboutInfoBar extends Component {
     return (
       <div>
         <h2 className="blog-post-header">About</h2>
-        <p id="about-infotext-main">
+        <p className="about-infotext-main">
           Hi! My name is Marc Philippe Beaujean, welcome to my web page and
           personal blog. I code things - more specifically: games, websites and
           software. I'm also a huge fan of statistics and machine learning.{" "}
         </p>
         <ReadMoreToggle contentId="aboutInfo">
-          <p id="about-infotext-main">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          <h3>About Me</h3>
+          <div className="about-infotext-main">
+            <ul className="about-list-keypoints">
+              <li>Country of Origin: Germany</li>
+              <li>Spoken Languages: Germany, English</li>
+              <li>Date of Birth: 1996</li>
+              <li>Programming Languages: C++, Python, JavaScript</li>
+              <li>
+                Academic Qualifications: International Baccalaureate, BSc(Hons)
+                Computer Games Applications Development
+              </li>
+            </ul>
+          </div>
         </ReadMoreToggle>
       </div>
     );
