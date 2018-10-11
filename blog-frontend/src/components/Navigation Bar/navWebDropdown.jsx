cde729de56ee14aff759d5af179c176f82b72f48
Marc Philippe Beaujean <marci.philippe@googlemail.com>
2018-10-11 17:51:09 +0100

    styled react calculator dropdown option

--- blog-frontend/src/components/Navigation Bar/navWebDropdown.jsx
+++ blog-frontend/src/components/Navigation Bar/navWebDropdown.jsx
@@ -3,6 +3,7 @@ import DropdownButton from "./dropdownButton";
 import SecondaryDropdownLayout from "./secondaryDropdownLayout";
 import "../../css/style.css";
 import { Link } from "react-router-dom";
+import ReactLogo from "../../media/react-logo.png";
 
 export default class NavWebDropdown extends Component {
   render() {
@@ -10,7 +11,12 @@ export default class NavWebDropdown extends Component {
       <SecondaryDropdownLayout sidebarId="web">
         <Link to="/react-calculator" style={{ textDecoration: "none" }}>
           <DropdownButton styleId="react-calculator">
-            <p>React Calculator</p>
+            <img src={ReactLogo} alt="React logo" id="react-logo" />
+            <p id="react-calculator-text">
+              React
+              <br />
+              Calculator
+            </p>
           </DropdownButton>
         </Link>
       </SecondaryDropdownLayout>
