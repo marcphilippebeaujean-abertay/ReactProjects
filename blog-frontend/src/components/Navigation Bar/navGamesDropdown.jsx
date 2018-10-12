0861df282446d5b7daaad22d41a435fb8dc16c1a
Marc Philippe Beaujean <marci.philippe@googlemail.com>
2018-10-12 18:53:29 +0100

    added new colour to show more toggle, created link to missing few

--- blog-frontend/src/components/Navigation Bar/navGamesDropdown.jsx
+++ blog-frontend/src/components/Navigation Bar/navGamesDropdown.jsx
@@ -3,12 +3,22 @@ import DropdownButton from "./dropdownButton";
 import SecondaryDropdownLayout from "./secondaryDropdownLayout";
 import "../../css/style.css";
 import SheTownLogo from "../../media/she-town-player.png";
+import TMFLogo from "../../media/tmf-logo.png";
 import { Link } from "react-router-dom";
 
 export default class NavGamesDropdown extends Component {
   render() {
     return (
       <SecondaryDropdownLayout sidebarId="games">
+        <a
+          href="https://www.themissingfew.com/"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          <DropdownButton styleId="tmf-dropdown">
+            <img src={TMFLogo} alt="The Missing Few Logo" id="tmf-logo" />
+          </DropdownButton>
+        </a>
         <Link to="/she-town" style={{ textDecoration: "none" }}>
           <DropdownButton styleId="she-town-dropdown">
             <img
